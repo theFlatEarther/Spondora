f70f013c1edffefd2a19b42d5fa5dd28589165b7
Bryce Pfingston <bryce.pfingston@gmail.com>
2021-08-30 16:16:48 -0600

    added songs

--- js/app.js
+++ js/app.js
@@ -30,3 +30,26 @@ function Song(name, artist, album, img=null) {
 // __________________________________ Event Listeners  __________________________________ 
 
 // __________________________________ Calls __________________________________ 
+
+Song(name, artist, album, img?
+
+  new Song ('STAY'; 'The Kid LAROI & Justin Bieber', 'F*CK LOVE 3: OVER YOU',Insta)
+  new Song ('Low', 'Flo Rida & T-Pain', 'Mail on Sunday', Insta)
+  new Song ('Because You Move Me', 'Tinlicker & Helsloot', 'Because You Move ', Insta)
+  new Song('One Dance', 'Drake', 'single', Insta )
+  new Song('Bad Guy','Billie Eilish', 'When We All Fall Asleep, Where Do We Go', Insta)
+  new Song ('Butter', 'BTS','n/a',Twitter)
+  new Song ('Stronger', 'Kanye West','Graduation',Twitter)
+  new Song ('Crazy In Love', 'Beyonce (feat. Jay-Z)','Dangerously In LoveDrake',Twitter)
+  new Song ('Wants and Needs','Drake (feat. Lil Baby)','Scary Hours 2', Twitter)
+  new Song ('Fireworks(I\'m The One)','ATEEZ','ZERO: FEVER Part.2Fac',Twitter)
+  new Song ('Jail','Kanye West','Donda',Facebook)
+  new Song ('INDUSTRY BABY','Lil Nax X & Jack Harlow','INDUSTRY BABY (feat. Jack Harlows)',Facebook)
+  new Song ('Bad Habits','Ed Sheeran','Bad Habits',Facebook)
+  new Song ('Pepas','Farruko','Pepas',Facebook)
+  new Song ('good 4 u','Olivia Rodrigo','SOUR',Facebook)
+  new Song ('Bring It','Cobra Starship','While the City Sleeps, We Rule the Streets',Myspacmye)
+  new Song ('1985','bowling for Soup','A Hangover You Don\'t Deserve',Myspace)
+  new Song ('One Week','Barenaked Ladies','Stunt',Myspace)
+  new Song ('Misery Business','Paramore','Riot',Myspace)
+  new Song ('Facedown','The Red Jumpsuit Apparatus','Don\'t You Fake It',Myspace)
