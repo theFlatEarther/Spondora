4652d2391ccc51e8f9a10828f106b49d99b03d56
David Hecker <HECKERDW@GMAIL.COM>
2021-08-30 14:21:33 -0700

    Added function update and comments.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 'use strict';
+
+// let the console know about Angela's core belief system
 console.log('Flat earth is real - Angela Dzodzomoney')
 
 // __________________________________ Global Variables __________________________________ 
+
+// arrays of songs by question number
 const questionNumber1 = [];
 const questionNumber2 = [];
 const questionNumber3 = [];
@@ -11,12 +15,12 @@ const questionNumber6 = [];
 
 // __________________________________ Constructor Functions  __________________________________ 
 
-function Song(name, artist, album, questionNumber, img=null) {
+// create a song object, accept name, artist, album, and optional img artwork
+function Song(name, artist, album, img=null) {
   this.name = name;
   this.artist = artist;
   this.album = album;
   this.img = img;
-  questionNumber.push(this);
 }
 
 // __________________________________ Prototype Methods __________________________________ 
