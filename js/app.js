78b292c0753c4df831e7529e5decc8ed2510fd69
David Hecker <HECKERDW@GMAIL.COM>
2021-08-30 14:14:20 -0700

    Added Song constructor function.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
 'use strict';
 console.log('Flat earth is real - Angela Dzodzomoney')
 
+// __________________________________ Global Variables __________________________________ 
+const questionNumber1 = [];
+const questionNumber2 = [];
+const questionNumber3 = [];
+const questionNumber4 = [];
+const questionNumber5 = [];
+const questionNumber6 = [];
+
+// __________________________________ Constructor Functions  __________________________________ 
+
+function Song(name, artist, album, questionNumber, img=null) {
+  this.name = name;
+  this.artist = artist;
+  this.album = album;
+  this.img = img;
+  questionNumber.push(this);
+}
+
+// __________________________________ Prototype Methods __________________________________ 
+
+// __________________________________ Global Functions __________________________________ 
+
+// __________________________________ Event Listeners  __________________________________ 
+
+// __________________________________ Calls __________________________________ 
